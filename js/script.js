ccda3029f61c559c0d5f36e709de87183578d3d2
aditacolman <acolman@sagradoalcal.edu.ar>
2024-10-03 12:01:53 -0300

    Modificar texto en modal de 'ver más'

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Esconde widget de elfsight en la página principal
 document.addEventListener('DOMContentLoaded', function () {
     setTimeout(() => {
         const elfsightLink = document.querySelector('.widget-container a[href*="elfsight.com"]');
@@ -7,6 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }, 1000); // Espera 2 segundos
 });
 
+//Cambiar icono en las preguntas frecuentes
 function toggleIcon(button) {
     const icon = button.querySelector('i');
     if (icon.classList.contains('bi-plus-lg')) {
@@ -16,4 +18,34 @@ function toggleIcon(button) {
         icon.classList.remove('bi-dash-lg');
         icon.classList.add('bi-plus-lg');
     }
+}
+
+//Ampliar imagen de servicios
+let scale = 1;
+
+function ampliarImagen(imagen) {
+    const modal = document.getElementById("modal");
+    const imagenAmpliada = document.getElementById("imagen-ampliada");
+
+    modal.style.display = "block";
+    imagenAmpliada.src = imagen.src;
+    scale = 1; // Reiniciar el zoom al abrir la imagen
+    imagenAmpliada.style.transform = `scale(${scale})`; // Aplicar el zoom inicial
+
+    // Agregar el evento de rueda del mouse para zoom
+    imagenAmpliada.onwheel = (event) => {
+        event.preventDefault(); // Prevenir el desplazamiento de la página
+        if (event.deltaY < 0) {
+            scale += 0.1; // Acercar
+        } else {
+            scale = Math.max(1, scale - 0.1); // Alejar, pero no menos de 1
+        }
+        imagenAmpliada.style.transform = `scale(${scale})`; // Aplicar el zoom
+    };
+}
+
+function cerrarModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+    scale = 1; // Reiniciar el zoom al cerrar el modal
 }
