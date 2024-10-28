9238d9ba43c87f9af259a113416f5fc1e36ab611
xaduar <xduarte@sagradoalcal.edu.ar>
2024-10-28 10:24:33 -0300

    Subida de merge

--- js/script.js
+++ js/script.js
@@ -126,8 +126,8 @@ currentDate = document.querySelector(".current-date"),
 prevNextIcon = document.querySelectorAll(".icons span");
 
 // getting new date, current year and month
-let date = new Date(),
-currYear = date.getFullYear(),
+let date = new Date();
+currYear = date.getFullYear();
 currMonth = date.getMonth();
 
 // storing full name of all months in array
